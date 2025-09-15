c2df0d1db645f18ecda362dfb83db5d885b76059
Kumar Kartikay <krkartikay@gmail.com>
2025-09-15 14:22:57 +0530

    implement single layer manual solve

--- manual_solve.py
+++ manual_solve.py
@@ -1,13 +1,28 @@
-from berghain_env import BerghainEnv
+import random
 import numpy as np
+from tqdm import tqdm
+from berghain_env import BerghainEnv
 
 env = BerghainEnv()
 
-WEIGHTS = [0.5, 0.3, 0.2, 0.1, 0.1]
+WEIGHTS = [[0, 0, 0, 0, 0],   # accept
+           [0, 0, 0, 0, 0]]   # reject
+BIAS = [0, 0]
 
+def softmax(logits):
+    # subtract max for numerical stability
+    exps = np.exp(logits - np.max(logits))
+    return exps / np.sum(exps)
 
 def choose_action(state):
-    pass
+    # one neuron that predicts 0 or 1
+    # logits = relu(weights * state + bias)
+    state_np = np.array(state)
+    logits = np.dot(WEIGHTS, state_np) + BIAS
+    logits = np.maximum(0, logits)  # relu
+    probs = softmax(logits)
+    choice = random.choices([0, 1], weights=probs)
+    return choice[0]
     
 def run_eps():
     state, info = env.reset()
@@ -15,18 +30,22 @@ def run_eps():
     total_reward = 0
 
     while True:
-        print("State:", state)
         action = choose_action(state)
-        state, reward, terminated, truncated, info = env.step(action)
+        new_state, reward, terminated, truncated, info = env.step(action)
+        # print(f"{step} State: {state}, Action: {action}, Reward: {reward}, Total Reward: {total_reward}.")
+        state = new_state
         step += 1
         total_reward += reward
-        print(f"{step} State: {state}, Action: {action}, Reward: {reward}, Total Reward: {total_reward}.")
         if terminated or truncated:
-            print(f"{terminated=} {truncated=}")
+            # print(f"{terminated=} {truncated=}")
             return total_reward
 
 def main():
     print("Starting manual run...")
-    run_eps()
+    total_reward = 0
+    for i in tqdm(range(1000)):
+        ep_rw = run_eps()
+        total_reward += ep_rw
+    print(f"Average Reward: {total_reward / 1000}")
 
 main()
